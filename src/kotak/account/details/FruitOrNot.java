23fa3b065e688bba80f70afa36934f0c958213e5
itsmeshya <112876181+itsmeshya@users.noreply.github.com>
2023-04-12 11:34:40 -0400

    Update FruitOrNot.java

--- src/kotak/account/details/FruitOrNot.java
+++ src/kotak/account/details/FruitOrNot.java
@@ -13,6 +13,9 @@ public class FruitOrNot {
     	case "Tomato":
     		System.out.println(fruit + " is a fruit");
     		break;
+	case "Brinjal":
+    		System.out.println(fruit + " is a fruit");
+    		break;		
     	default:
     		System.out.println("Not sure about this fruit");
     	}
